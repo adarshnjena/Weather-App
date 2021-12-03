00da41c2a0b035ec4eb7464e54e50b5c154938c4
Adarsh Jena <76236137+adarshnjena@users.noreply.github.com>
2021-12-03 18:51:56 +0530

    commit

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\flutter_proj\Clima\Clima-Flutter-master"
+export "FLUTTER_APPLICATION_PATH=D:\flutter_proj\Weather-App"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
